97a01efc18ffd6fd8119b9decbf9a65a3e948d4d
Denis <denis.petrov56245@gmail.com>
2023-07-07 22:50:47 +0200

    new

--- Main.java
+++ Main.java
@@ -2,15 +2,11 @@
 
 public class Main {
     public static void main(String[] args) {
-
          Main m = new Main();
          m.drawShape(new Qadr(10));
          m.drawShape(new Circle(17));
          m.drawShape(new Rectangle(12, 10));
-//         m.drawShape(new Elipse());
-//         m.drawShape(new Sqare());
-//         m.drawShape(new Rectangle());
-
+         m.drawShape(new Oval(14, 26));
 
     }
 
--- Oval.java
+++ Oval.java
@@ -1,7 +1,19 @@
-import java.util.Random;
 
 public class Oval extends Circle{
-    public Oval(int radius) {
+
+    private int radius2;
+
+    public Oval(int radius, int radius2) {
         super(radius);
+        this.radius2 = radius2;
+    }
+
+    public int getRadius2() {
+        return radius2;
+    }
+
+    @Override
+    public String getName() {
+        return String.format("Oval {radius  = %d, circle = %d}", "Rectangle", getRadius(), getRadius2());
     }
 }
