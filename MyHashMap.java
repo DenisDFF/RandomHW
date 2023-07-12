37d6a8945ad3052f3e1585378ddc5f5079f8b725
Denis <denis.petrov56245@gmail.com>
2023-07-12 17:46:26 +0200

    final? x2

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,99 +1,98 @@
 class MyHashMap<K, V> {
 
-    private Entry<K,V>[] table;
-    private int capacity= 4;
+    private Entry<K, V>[] table;
+    private int capacity = 4;
 
     static class Entry<K, V> {
         K key;
         V value;
-        Entry<K,V> next;
+        Entry<K, V> next;
 
-        public Entry(K key, V value, Entry<K,V> next){
+        public Entry(K key, V value, Entry<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
     }
 
-
     @SuppressWarnings("unchecked")
-    public MyHashMap(){
+    public MyHashMap() {
         table = new Entry[capacity];
     }
 
-    public void put(K newKey, V data){
-        if(newKey==null)
-            return;    //does not allow to store null.
+    public void put(K newKey, V data) {
+        if (newKey == null)
+            return; // does not allow storing null.
 
-        //calculate hash of key.
-        int hash=hash(newKey);
-        //create new entry.
-        Entry<K,V> newEntry = new Entry<K,V>(newKey, data, null);
+        ensureCapacity();
 
-        //if table location does not contain any entry, store entry there.
-        if(table[hash] == null){
+        // calculate hash of key.
+        int hash = hash(newKey);
+        // create new entry.
+        Entry<K, V> newEntry = new Entry<K, V>(newKey, data, null);
+
+        // if table location does not contain any entry, store entry there.
+        if (table[hash] == null) {
             table[hash] = newEntry;
-        }else{
-            Entry<K,V> previous = null;
-            Entry<K,V> current = table[hash];
+        } else {
+            Entry<K, V> previous = null;
+            Entry<K, V> current = table[hash];
 
-            while(current != null){ //we have reached last entry of bucket.
-                if(current.key.equals(newKey)){
-                    if(previous==null){  //node has to be insert on first of bucket.
-                        newEntry.next=current.next;
-                        table[hash]=newEntry;
+            while (current != null) { // we have reached last entry of bucket.
+                if (current.key.equals(newKey)) {
+                    if (previous == null) { // node has to be insert on first of bucket.
+                        newEntry.next = current.next;
+                        table[hash] = newEntry;
                         return;
-                    }
-                    else{
-                        newEntry.next=current.next;
-                        previous.next=newEntry;
+                    } else {
+                        newEntry.next = current.next;
+                        previous.next = newEntry;
                         return;
                     }
                 }
-                previous=current;
+                previous = current;
                 current = current.next;
             }
             previous.next = newEntry;
         }
     }
 
-    public V get(K key){
+    public V get(K key) {
         int hash = hash(key);
-        if(table[hash] == null){
+        if (table[hash] == null) {
             return null;
-        }else{
-            Entry<K,V> temp = table[hash];
-            while(temp!= null){
-                if(temp.key.equals(key))
+        } else {
+            Entry<K, V> temp = table[hash];
+            while (temp != null) {
+                if (temp.key.equals(key))
                     return temp.value;
-                temp = temp.next; //return value corresponding to key.
+                temp = temp.next; // return value corresponding to key.
             }
-            return null;   //returns null if key is not found.
+            return null; // returns null if key is not found.
         }
     }
 
-    public boolean remove(K deleteKey){
+    public boolean remove(K deleteKey) {
 
-        int hash=hash(deleteKey);
+        int hash = hash(deleteKey);
 
-        if(table[hash] == null){
+        if (table[hash] == null) {
             return false;
-        }else{
-            Entry<K,V> previous = null;
-            Entry<K,V> current = table[hash];
+        } else {
+            Entry<K, V> previous = null;
+            Entry<K, V> current = table[hash];
 
-            while(current != null){ //we have reached last entry node of bucket.
-                if(current.key.equals(deleteKey)){
-                    if(previous==null){  //delete first entry node.
-                        table[hash]=table[hash].next;
+            while (current != null) { // we have reached last entry node of bucket.
+                if (current.key.equals(deleteKey)) {
+                    if (previous == null) { // delete first entry node.
+                        table[hash] = table[hash].next;
                         return true;
-                    }
-                    else{
-                        previous.next=current.next;
+                    } else {
+                        previous.next = current.next;
                         return true;
                     }
                 }
-                previous=current;
+                previous = current;
                 current = current.next;
             }
             return false;
@@ -101,24 +100,55 @@ class MyHashMap<K, V> {
 
     }
 
-    public void display(){
+    public void display() {
 
-        for(int i=0;i<capacity;i++){
-            if(table[i]!=null){
-                Entry<K, V> entry=table[i];
-                while(entry!=null){
-                    System.out.print("{"+entry.key+"="+entry.value+"}" +" ");
-                    entry=entry.next;
+        for (int i = 0; i < capacity; i++) {
+            if (table[i] != null) {
+                Entry<K, V> entry = table[i];
+                while (entry != null) {
+                    System.out.print("{" + entry.key + "=" + entry.value + "}" + " ");
+                    entry = entry.next;
                 }
             }
         }
 
     }
 
-    private int hash(K key){
+    private int hash(K key) {
         return Math.abs(key.hashCode()) % capacity;
     }
 
+    private void ensureCapacity() {
+        if (size() >= capacity * 0.75) {
+            resize();
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        capacity *= 2;
+        Entry<K, V>[] newTable = new Entry[capacity];
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                Entry<K, V> next = entry.next;
+                int hash = hash(entry.key);
+                entry.next = newTable[hash];
+                newTable[hash] = entry;
+                entry = next;
+            }
+        }
+        table = newTable;
+    }
+
+    public int size() {
+        int count = 0;
+        for (Entry<K, V> entry : table) {
+            while (entry != null) {
+                count++;
+                entry = entry.next;
+            }
+        }
+        return count;
+    }
+
 }
-
-
