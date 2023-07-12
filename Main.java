107e0b5ac11b7127453dc4da9f6ae637d1240381
Denis <denis.petrov56245@gmail.com>
2023-07-12 17:21:59 +0200

    final?

--- Main.java
+++ Main.java
@@ -1,20 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        MyArrayList list = new MyArrayList();
+        MyArrayList<String> list = new MyArrayList<>();
 
-        // Добавление элементов в список
         list.add("Apple");
         list.add("Banana");
         list.add("Orange");
 
         System.out.println("Size: " + list.size());
 
-        // Вывод элементов списка
         for (int i = 0; i < list.size(); i++) {
             System.out.println("Element at index " + i + ": " + list.get(i));
         }
 
-        // Удаление элемента из списка
         list.remove(1);
 
         System.out.println("Size after removal: " + list.size());
@@ -23,12 +20,11 @@ public class Main {
             System.out.println("Element at index " + i + ": " + list.get(i));
         }
 
-        // Очистка списка
         list.clear();
 
         System.out.println("Size after clearing: " + list.size());
 
-        // Добавление элементов в список
+
         list.add("A");
         list.add("B");
         list.add("C");
@@ -36,20 +32,40 @@ public class Main {
 
         System.out.println("Размер списка: " + list.size());
 
-        // Получение элемента по индексу
-        Object element = list.get(2);
+
+        String element = list.get(2);
         System.out.println("Элемент по индексу 2: " + element);
 
-        // Удаление элемента по индексу
+
         list.remove(1);
         System.out.println("Размер списка после удаления элемента: " + list.size());
 
-        // Очистка списка
+
         list.clear();
         System.out.println("Размер списка после очистки: " + list.size());
 
-        // Добавление элементов в стек
-        MyStack stack = new MyStack();
+        list.add("10");
+        list.add("20");
+        list.add("30");
+        list.add("40");
+
+        // Вывод размера списка
+        System.out.println("Размер списка: " + list.size());
+
+        // Вывод первого элемента в списке
+        System.out.println("Первый элемент в списке: " + list.get(0));
+
+        // Вывод размера списка после удаления элемента
+        System.out.println("Размер списка: " + list.size());
+
+        // Очистка списка
+        list.clear();
+
+        // Вывод размера списка после очистки
+        System.out.println("Размер списка после очистки: " + list.size());
+
+
+        MyStack<String> stack = new MyStack<>();
 
         stack.push("Apple");
         stack.push("Banana");
@@ -57,26 +73,31 @@ public class Main {
 
         System.out.println("Size: " + stack.size());
 
-        // Получение элемента с вершины стека (первый элемент в стеке)
         System.out.println("Peek: " + stack.peek());
-
-        // Удаление элемента с вершины стека (первый элемент в стеке)
         System.out.println("Pop: " + stack.pop());
 
         System.out.println("Size after pop: " + stack.size());
 
-        // Создание и использование объекта MyHashMap
-        MyHashMap map = new MyHashMap();
 
-        map.put("key1", "value1");
-        map.put("key2", "value2");
-        map.put("key3", "value3");
+        MyHashMap<String, Integer> hashMap = new MyHashMap<>();
 
-        System.out.println("Size of map: " + map.size());
-        System.out.println("Value for key2: " + map.get("key2"));
+        hashMap.put("Apple", 10);
+        hashMap.put("Banana", 20);
+        hashMap.put("Orange", 30);
 
-        map.remove("key1");
+        System.out.println("Size of HashMap: " + list.size());
 
-        System.out.println("Size of map after removal: " + map.size());
+        System.out.println("Value for 'Apple': " + hashMap.get("Apple"));
+        System.out.println("Value for 'Banana': " + hashMap.get("Banana"));
+        System.out.println("Value for 'Orange': " + hashMap.get("Orange"));
+
+        hashMap.remove("Banana");
+
+        System.out.println("Size after removal: " + list.size());
+        System.out.println("Value for 'Banana' after removal: " + hashMap.get("Banana"));
+
+        list.clear();
+
+        System.out.println("Size after clearing: " + list.size());
     }
 }
--- MyHashMap.java
+++ MyHashMap.java
@@ -1,99 +1,124 @@
-public class MyHashMap<K, V> {
-    private static final int DEFAULT_CAPACITY = 16;
-    private Node<K, V>[] buckets;
-    private int size;
+class MyHashMap<K, V> {
 
-    public MyHashMap() {
-        buckets = new Node[DEFAULT_CAPACITY];
-        size = 0;
-    }
+    private Entry<K,V>[] table;
+    private int capacity= 4;
 
-    public void put(K key, V value) {
-        int bucketIndex = getBucketIndex(key);
-        Node<K, V> newNode = new Node<>(key, value);
+    static class Entry<K, V> {
+        K key;
+        V value;
+        Entry<K,V> next;
 
-        if (buckets[bucketIndex] == null) {
-            buckets[bucketIndex] = newNode;
-            size++;
-        } else {
-            Node<K, V> currentNode = buckets[bucketIndex];
-            Node<K, V> prevNode = null;
-
-            while (currentNode != null) {
-                if (currentNode.key.equals(key)) {
-                    currentNode.value = value;
-                    return;
-                }
-                prevNode = currentNode;
-                currentNode = currentNode.next;
-            }
-
-            prevNode.next = newNode;
-            size++;
-        }
-    }
-
-    public void remove(K key) {
-        int bucketIndex = getBucketIndex(key);
-
-        Node<K, V> currentNode = buckets[bucketIndex];
-        Node<K, V> prevNode = null;
-
-        while (currentNode != null) {
-            if (currentNode.key.equals(key)) {
-                if (prevNode == null) {
-                    buckets[bucketIndex] = currentNode.next;
-                } else {
-                    prevNode.next = currentNode.next;
-                }
-                size--;
-                return;
-            }
-            prevNode = currentNode;
-            currentNode = currentNode.next;
-        }
-    }
-
-    public void clear() {
-        for (int i = 0; i < buckets.length; i++) {
-            buckets[i] = null;
-        }
-        size = 0;
-    }
-
-    public int size() {
-        return size;
-    }
-
-    public V get(K key) {
-        int bucketIndex = getBucketIndex(key);
-
-        Node<K, V> currentNode = buckets[bucketIndex];
-
-        while (currentNode != null) {
-            if (currentNode.key.equals(key)) {
-                return currentNode.value;
-            }
-            currentNode = currentNode.next;
-        }
-
-        return null;
-    }
-
-    private int getBucketIndex(K key) {
-        int hashCode = key.hashCode();
-        return Math.abs(hashCode) % buckets.length;
-    }
-
-    private static class Node<K, V> {
-        private K key;
-        private V value;
-        private Node<K, V> next;
-
-        public Node(K key, V value) {
+        public Entry(K key, V value, Entry<K,V> next){
             this.key = key;
             this.value = value;
-            this.next = null;
+            this.next = next;
         }
     }
+
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap(){
+        table = new Entry[capacity];
+    }
+
+    public void put(K newKey, V data){
+        if(newKey==null)
+            return;    //does not allow to store null.
+
+        //calculate hash of key.
+        int hash=hash(newKey);
+        //create new entry.
+        Entry<K,V> newEntry = new Entry<K,V>(newKey, data, null);
+
+        //if table location does not contain any entry, store entry there.
+        if(table[hash] == null){
+            table[hash] = newEntry;
+        }else{
+            Entry<K,V> previous = null;
+            Entry<K,V> current = table[hash];
+
+            while(current != null){ //we have reached last entry of bucket.
+                if(current.key.equals(newKey)){
+                    if(previous==null){  //node has to be insert on first of bucket.
+                        newEntry.next=current.next;
+                        table[hash]=newEntry;
+                        return;
+                    }
+                    else{
+                        newEntry.next=current.next;
+                        previous.next=newEntry;
+                        return;
+                    }
+                }
+                previous=current;
+                current = current.next;
+            }
+            previous.next = newEntry;
+        }
+    }
+
+    public V get(K key){
+        int hash = hash(key);
+        if(table[hash] == null){
+            return null;
+        }else{
+            Entry<K,V> temp = table[hash];
+            while(temp!= null){
+                if(temp.key.equals(key))
+                    return temp.value;
+                temp = temp.next; //return value corresponding to key.
+            }
+            return null;   //returns null if key is not found.
+        }
+    }
+
+    public boolean remove(K deleteKey){
+
+        int hash=hash(deleteKey);
+
+        if(table[hash] == null){
+            return false;
+        }else{
+            Entry<K,V> previous = null;
+            Entry<K,V> current = table[hash];
+
+            while(current != null){ //we have reached last entry node of bucket.
+                if(current.key.equals(deleteKey)){
+                    if(previous==null){  //delete first entry node.
+                        table[hash]=table[hash].next;
+                        return true;
+                    }
+                    else{
+                        previous.next=current.next;
+                        return true;
+                    }
+                }
+                previous=current;
+                current = current.next;
+            }
+            return false;
+        }
+
+    }
+
+    public void display(){
+
+        for(int i=0;i<capacity;i++){
+            if(table[i]!=null){
+                Entry<K, V> entry=table[i];
+                while(entry!=null){
+                    System.out.print("{"+entry.key+"="+entry.value+"}" +" ");
+                    entry=entry.next;
+                }
+            }
+        }
+
+    }
+
+    private int hash(K key){
+        return Math.abs(key.hashCode()) % capacity;
+    }
+
 }
+
+
