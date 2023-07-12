346678f74bae86825cc3ea42117c63c6ccc273be
Denis <denis.petrov56245@gmail.com>
2023-07-12 21:00:02 +0200

    final? x5

--- MyHashMap.java
+++ MyHashMap.java
@@ -149,13 +149,3 @@ class MyHashMap<K, V> {
     }
 }
 
-class Main {
-    public static void main(String[] args) {
-        MyHashMap<Integer, Integer> map = new MyHashMap<>();
-        for (int i = 0; i < 1000; i++) {
-            map.put(i, i);
-        }
-
-        System.out.println(map.size());
-    }
-}
